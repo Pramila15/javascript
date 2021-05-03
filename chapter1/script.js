a075d03752ebc8b9db350ce52cf1416326476836
Pramila15 <53640792+Pramila15@users.noreply.github.com>
2021-05-03 20:05:54 +0530

    Add files via upload

---
+++ chapter1/script.js
@@ -0,0 +1,47 @@
+//Alert
+alert('hello world')
+
+//VAR , LET , CONST
+let age = 25;
+let year = 2000;
+console.log(age, year);
+
+age = 75;
+//change the value
+console.log(age);
+
+//old way of creating variable
+var score = 75;
+console.log(score);
+
+//camelCase
+let myAge = 25;
+console.log(myAge);
+
+//cant override value
+const points = 50;
+points = 20;
+console.log(points);
+
+// Single line comment
+/* Multiline comment */
+
+//Data Types
+/*
+1. Number 
+2. String
+3. Boolean
+4. Null
+5. Undefined
+6. Object
+7. Symbol
+*/
+
+//Strings
+console.log('hello world');
+let email = 'pramila@gmail.com';
+console.log(email);
+
+let firstName = 'Pramila';
+let lastName = 'Dalavai';
+
